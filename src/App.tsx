f747f2c7110a297fb5c6f155b48a1072e6ae8101
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-02-08 09:51:50 -0300

    Created React App with TypeScript and installed packages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #696;
+  font-size: 3rem;
+`;
+
+export function App() {
+  return (
+    <div className="App">
+      <Title>Hello World</Title>
+    </div>
+  );
+}
